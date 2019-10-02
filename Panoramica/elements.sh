c463acab401b39c921a11121e5e9a4a739265593
Nicolò Trevisani <Nicolo@MacBook-Air-di-Nicolo.local>
2019-10-02 14:56:59 +0200

    first working version

--- Panoramica/elements.sh
+++ Panoramica/elements.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-# curl http://swcarpentry.github.io/shell-novice/data/data-shell.zip -o data-shell.zip
-# unzip data-shell.zip
-mkdir -p elements_by_atomic_number
-#find data-shell -name *.xml
-#elements=$(find data-shell -name *.xml)
-#echo $elements
+curl http://swcarpentry.github.io/shell-novice/data/data-shell.zip -o data-shell.zip
+unzip data-shell.zip
+outputFolder=elements_by_atomic_number
+mkdir -p $outputFolder
 for element in $(find data-shell -name *.xml)
 do
-#echo $element | sed -e 's/data-shell\/data\/elements\// /' | sed -e 's/.xml/ /'
 atomicNumber=$(cat $element | grep atomic-number | sed -e 's/<atomic-number>\(.*\)<\/atomic-number>/\1/')
-echo $atomicNumber
+characterNumber=$(echo -n $atomicNumber | wc -c)
 elementName=$(cat $element | grep "<element name" | sed -e 's/<element name="\(.*\)"\/>/\1/' )
-echo $elementName
+if [ $characterNumber -eq 1 ]; then
+    newAtomicNumber=$(echo $atomicNumber | sed -e 's/^/00/')
+fi
+if [ $characterNumber -eq 2 ]; then
+    newAtomicNumber=$(echo $atomicNumber | sed -e 's/^/0/')
+fi
+if [ $characterNumber -eq 3 ]; then
+    newAtomicNumber=$(echo $atomicNumber)
+fi
+newFileName=$newAtomicNumber\_$elementName.xml
+cp $element $outputFolder/$newFileName
 done
-
+rm data-shell.zip
+rm -r data-shell
+ls -lrt elements_by_atomic_number
+echo All done!
