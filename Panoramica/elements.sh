59d657ee56ac2520e738444b375cfc5dc634e6bd
Nicolò Trevisani <Nicolo@MacBook-Air-di-Nicolo.local>
2019-10-03 10:47:39 +0200

    maybe a bit more readable

--- Panoramica/elements.sh
+++ Panoramica/elements.sh
@@ -2,23 +2,32 @@
 URL=${1:-http://swcarpentry.github.io/shell-novice/data/data-shell.zip}
 DOWNLOAD_NAME=${2:-data-shell.zip}
 OUTPUT_FOLDER=${3:-elements_by_atomic_number}
+
 # downloading the zip file
 curl $URL -o $DOWNLOAD_NAME
+
 # unzipping
 unzip data-shell.zip
+
 # creating destination folder
 mkdir -p $OUTPUT_FOLDER
+
 # counting how many xml files we have
 initialFilesNumber=$(find data-shell -name *.xml | wc -l)
+
 # loop over all the xml files to modify their names
 for element in $(find data-shell -name *.xml)
 do
+
 # retreiving the atomic number
 atomicNumber=$(cat $element | grep atomic-number | sed -e 's/<atomic-number>\(.*\)<\/atomic-number>/\1/')
+
 # counting how many digits we have in the atomic number (to fill with 0 on the left if less than 3)
 characterNumber=$(echo -n $atomicNumber | wc -c)
+
 # retreiving the element name
 elementName=$(cat $element | grep "<element name" | sed -e 's/<element name="\(.*\)"\/>/\1/' )
+
 # now making all the atomic number have 3 digits
 if [ $characterNumber -eq 1 ]; then
     newAtomicNumber=$(echo $atomicNumber | sed -e 's/^/00/')
@@ -29,16 +38,21 @@ fi
 if [ $characterNumber -eq 3 ]; then
     newAtomicNumber=$(echo $atomicNumber)
 fi
+
 # creating the new file name
 newFileName=$newAtomicNumber\_$elementName.xml
+
 # copying the file in the new folder with the new name
 cp $element $OUTPUT_FOLDER/$newFileName
 done
+
 # removing unnecessary stuff
 rm data-shell.zip
 rm -r data-shell
+
 # showing elements in the new folder
 ls -1 elements_by_atomic_number
+
 # counting how many xml files have been copied to the new folder
 finalFilesNumber=$(ls -1 elements_by_atomic_number | wc -l)
 echo ===========
@@ -47,6 +61,7 @@ if [ $finalFilesNumber -eq $initialFilesNumber ]; then
 else
     echo Something went wrong! :\'\(
 fi
+
 # some final suggestions
 echo ===========
 echo I am copying the $URL url into the $DOWNLOAD_NAME file, and copying the xml files, with the requested name, in the $OUTPUT_FOLDER folder
