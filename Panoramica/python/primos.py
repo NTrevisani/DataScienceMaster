b438c90f61e3016cd52d2596cecfa4d05f26c8af
Nicolò Trevisani <Nicolo@MacBook-Air-di-Nicolo.local>
2019-10-04 11:17:17 +0200

    numeros primos

---
+++ Panoramica/python/primos.py
@@ -0,0 +1,20 @@
+# El siguiente código debería obtener los números primos que
+# existan hasta "n", incluyendo "n", pero contiene errores.
+
+n = 30
+
+# Inicializo la lista de numeros primos
+primos = []
+primos.append(1)
+primos.append(2)
+
+# Itero sobre los numeros
+for j in range(2,n+1):
+    isPrime = True
+    for i in primos[1:]:
+        # check if not divisible by elem of list
+        if j % i == 0:                
+            isPrime = False
+    if isPrime == True:
+        primos.append(j)
+print(primos)
