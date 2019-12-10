6e75e2e951b40cacb478dbd52197ffcab6b7bd04
NTrevisani <nicolo.trevisani89@gmail.com>
2019-12-10 18:18:01 +0100

    slightly better

--- DataLifeCycle/TrabajoGrupo/TrabajoGrupo.py
+++ DataLifeCycle/TrabajoGrupo/TrabajoGrupo.py
@@ -5,7 +5,7 @@
 # 
 # https://datosabiertos.malaga.eu/recursos/ambiente/calidadaire/2018.json
 
-# In[17]:
+# In[177]:
 
 
 # Load the Pandas libraries with alias 'pd' 
@@ -18,43 +18,70 @@ calidad_aire = pd.read_csv("calidad_aire_2018.csv")
     
 # Preview the first 5 lines of the loaded data 
 calidad_aire.head()
+list(calidad_aire)
 
 # Remove repeated/useless columns 
-calidad_aire = calidad_aire.drop(calidad_aire.columns[[0,1,2,3,4,5,6,7,8]], axis=1)
+calidad_aire = calidad_aire.drop(calidad_aire.columns[[0,1,2,3,4,5,6,7,8,11,18]], axis=1)
 calidad_aire.head()
 
 
-# In[18]:
+# In[178]:
 
 
-list(calidad_aire)
+list(calidad_aire)[2:]
 
 
-# In[102]:
+# In[179]:
+
+
+droplist = calidad_aire.dtypes != object
+droplist[0:2] = False
+droplist
+
+
+# In[180]:
+
+
+calidad_aire_num = calidad_aire.drop(calidad_aire.columns[calidad_aire.dtypes == object], axis=1)
+calidad_aire_num.head()
+
+
+# In[181]:
+
+
+droplist = calidad_aire.dtypes != object
+droplist[0:2] = False
+droplist
+
+calidad_aire_obj = calidad_aire.drop(calidad_aire.columns[droplist], axis=1)
+calidad_aire_obj.head()
+
+
+# In[182]:
 
 
 # Pinto una variable numerica: BIEN
 
-calidad_aire.plot.scatter(x = 'geometry/coordinates/0/4/0', 
+calidad_aire_num.plot.scatter(x = 'geometry/coordinates/0/4/0', 
             y = 'geometry/coordinates/0/4/1', 
             c = 'properties/pm1',
             colormap='viridis',
             s = 5)
 
 
-# In[86]:
+# In[183]:
 
 
 all_z_variables = list(calidad_aire)[3:]
 print(all_z_variables)
 
 
-# In[103]:
+# In[184]:
 
 
-# Intento hacer un loop sobre todas las variables, numericas y categoricas: MAL!
+# Intento hacer un loop sobre todas las variables numericas BIEN!
 
-for var in calidad_aire.columns[6:]:
+for var in calidad_aire_num.columns[6:]:
 
     print(var)    
     
@@ -63,6 +90,32 @@ for var in calidad_aire.columns[6:]:
             c = var, #calidad_aire['properties/pm1'], #'properties/pm1',
             colormap='viridis',
             s = 5)
+
+
+# In[189]:
+
+
+# Intento hacer un loop sobre todas las variables categoricas
+
+for var in calidad_aire_obj.columns[2:]:
+
+    print(calidad_aire_obj[var].unique())    
+    colors = np.array(calidad_aire_obj[var].unique())
+    
+    calidad_aire.plot.scatter(x = 'geometry/coordinates/0/4/0', 
+        y = 'geometry/coordinates/0/4/1', 
+        c = colors[var], #calidad_aire['properties/pm1'], #'properties/pm1',
+        colormap='viridis',
+        s = 5)
+    
+    
+    #plt.scatter(x,y, c=colors[z])
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
