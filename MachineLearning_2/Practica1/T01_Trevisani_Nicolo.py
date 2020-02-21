83be1ad97ab88931d3a5c1c8ca30a068ca0a3ae9
NTrevisani <nicolo.trevisani89@gmail.com>
2020-02-21 11:29:25 +0100

    done

--- MachineLearning_2/Practica1/T01_Trevisani_Nicolo.py
+++ MachineLearning_2/Practica1/T01_Trevisani_Nicolo.py
@@ -37,8 +37,8 @@ for var in rain.columns:
 rain.RainTomorrow[rain.RainTomorrow == 'No'] = 0
 rain.RainTomorrow[rain.RainTomorrow == 'Yes'] = 1
 
-# Now, convert it into float
-rain.RainTomorrow = rain.RainTomorrow.astype(float)
+# Now, convert it into integer
+rain.RainTomorrow = rain.RainTomorrow.astype(int)
 rain.dtypes
 
 
@@ -86,13 +86,19 @@ cb = plt.colorbar()
 cb.ax.tick_params(labelsize=14)
 
 
+# The correlation is high between some of the variables:
+# - MinTemp - Temp9am
+# - MaxTemp - Temp3pm
+# - Pressure9am - Pressure3pm
+# 
+# Additionally, some of them are discrete
+# - Cloud9am;
+# - Cloud3pm.
+# 
+# I remove the highly correlated variables and the discrete ones, to make the model simpler.
+
 # In[8]:
 
-
-# The correlation is high between some of the variables:
-# MinTemp - Temp9am
-# MaxTemp - Temp3pm
-# Pressure9am - Pressure3pm
 
 droplist2 = ["Temp9am", "Temp3pm", "Pressure3pm", "Cloud9am", "Cloud3pm"]
 brisbane_slim = brisbane_slim.drop(droplist2, axis = 1)
@@ -121,10 +127,15 @@ brisbane_slim.head()
 # In[11]:
 
 
+# 2D visualization of a pair of variables:
 # MinTemp vs Humidity3pm
-plt.scatter(brisbane_slim.iloc[0:200, 0], 
-            brisbane_slim.iloc[0:200, 7], 
-            c=brisbane_slim.iloc[0:200, -1], 
+nEvents = 500
+if nEvents > brisbane_slim.shape[0]: 
+    nEvents = brisbane_slim.shape[0]
+
+plt.scatter(brisbane_slim.iloc[0:nEvents, 0], 
+            brisbane_slim.iloc[0:nEvents, 7], 
+            c=brisbane_slim.iloc[0:nEvents, -1], 
             s=50, 
             cmap='bwr');
 
@@ -137,9 +148,82 @@ import seaborn as sns
 sns.pairplot(brisbane_slim, hue = 'RainTomorrow')
 
 
-# ### 3. Separa los datos en un conjunto de training y otro de test.
+# ### Now let's try some more fancy 3D visualization
 
 # In[13]:
+
+
+# load Plotly
+from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
+import plotly.graph_objs as go
+
+# initiate the Plotly Notebook mode
+init_notebook_mode(connected=True)
+
+
+# In[14]:
+
+
+# define a function for 3D plotting using Plotly
+def plot_3D(X, y):
+    
+    # retrieve indices of zeroes and ones
+    idx0 = y==0
+    idx1 = y==1
+    
+    # define data set 1 plot options
+    X0 = X[idx0]
+    trace0 = go.Scatter3d(x=X0.iloc[:,0], y=X0.iloc[:,1], z=X0.iloc[:,2],
+        mode='markers',
+        marker=dict(size=6, color='blue', opacity=0.8)
+    )
+
+    # define data set 2 plot options
+    X1 = X[idx1]
+    trace1 = go.Scatter3d(x=X1.iloc[:,0], y=X1.iloc[:,1], z=X1.iloc[:,2],
+        mode='markers',
+        marker=dict(size=6, color='red', opacity=0.8)
+    )
+
+    # set aspect ratio
+    scene = dict(aspectmode="manual", aspectratio=dict(x=1, y=1, z=1))
+
+    # define figure properties
+    layout = go.Layout(
+        scene=scene,
+        height=600,
+        width=900
+    )
+
+    # produce the plot
+    fig = go.Figure(data=[trace0,trace1],layout=layout)
+    iplot(fig)
+
+
+# In[15]:
+
+
+# plot the data in 3D
+
+nEvents = 500
+if nEvents > brisbane_slim.shape[0]: 
+    nEvents = brisbane_slim.shape[0]
+
+# I'm plotting 3 variables at the same time:
+# 0: MinTemp
+# 6: Humidity9am 
+# 7: Hummidity3pm
+
+Phi = brisbane_slim.iloc[0:nEvents,[0,6,7]]
+
+y = brisbane_slim.iloc[0:nEvents, -1]
+
+plot_3D(Phi,y)
+
+
+# ### 3. Separa los datos en un conjunto de training y otro de test.
+
+# In[16]:
 
 
 # To avoid large triaing time, I keep just 1000 events.
@@ -160,36 +244,17 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # ### 4. Entrena una SVM sobre los datos de training. Busca los parámetros óptimos usando GridSearchCV.
 
-# In[14]:
+# In[40]:
 
 
 from sklearn import datasets, svm
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
-
-# Uso el kernel gaussiano
-kernel = 'rbf'
-
-# fit the model
-clf = SVC(kernel='rbf')
-
-clf.get_params().keys()
-
-
-# In[21]:
-
-
 from sklearn.model_selection import GridSearchCV
 from time import time
 
-# Full grid: it takes too much time
-"""
-param_grid = {'C': [1E2, 5E2, 1E3, 5E3, 1E4],
-              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], 
-              'kernel': ['linear', 'rbf', 'poly'],}
-"""
-# This looks as a better compromise between detail in the grid and time needed
-param_grid = {'C': [1E1, 1E2, 5E2, 1E3, 5E3],
+# Define the grid
+param_grid = {'C': [1, 1E1, 1E2, 5E2, 1E3, 5E3],
               'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], 
               'kernel': ['rbf', 'linear'],}
 
@@ -206,14 +271,14 @@ print(grid.best_params_)
 
 # ### 5. Prueba el clasificador sobre los datos de test y reporta el resultado.
 
-# In[22]:
+# In[41]:
 
 
 # Evaluate the prediction of the SVM
 y_pred = clf.predict(X_test)
 
 
-# In[23]:
+# In[42]:
 
 
 # Print a summary of the performance
@@ -222,7 +287,7 @@ from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
 
 
-# In[24]:
+# In[43]:
 
 
 # Confusion matrix
@@ -231,7 +296,7 @@ from sklearn.metrics import confusion_matrix
 print(confusion_matrix(y_test, y_pred))
 
 
-# In[19]:
+# In[44]:
 
 
 # Confusion matrix, but slightly nicer
